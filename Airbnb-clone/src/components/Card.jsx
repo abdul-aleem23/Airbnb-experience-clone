5db496743c5d73735712fa1d4bb5a58e793317b4
abdul-aleem23 <abdul_aleem98@hotmail.com>
2023-01-24 18:43:15 +0100

    added cards

--- Airbnb-clone/src/components/Card.jsx
+++ Airbnb-clone/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Card() {
   return (
     <div className="Card--div">
-        <img className="Card--image" src="./src/images/card-image1.png"></img>
+      <img className="Card--image" src="./src/images/card-image1.png"></img>
       <div className="Card--stats">
         <img className="star-icon" src="./src/images/star-icon.png"></img>
         <span> 5.0</span>
